ec8f0dac4e52ed70d214ec88663a7a5fb8e52a9e
Paul Kitchener <p.a.kitchener@gmail.com>
2015-10-27 13:28:27 -0500

    Initial Commit

---
+++ InstantGramTests/InstantGramTests.m
@@ -0,0 +1,39 @@
+//
+//  InstantGramTests.m
+//  InstantGramTests
+//
+//  Created by Paul Kitchener on 10/27/15.
+//  Copyright © 2015 Paul Kitchener. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InstantGramTests : XCTestCase
+
+@end
+
+@implementation InstantGramTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
