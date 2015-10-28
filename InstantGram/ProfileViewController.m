c26f694f1b1d159f0b10e659b5ea133aed75bbdd
Paul Kitchener <p.a.kitchener@gmail.com>
2015-10-28 13:53:29 -0500

    updated tabs, tab images, and profileview controller

--- InstantGram/ProfileViewController.m
+++ InstantGram/ProfileViewController.m
@@ -8,7 +8,13 @@
 
 #import "ProfileViewController.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UITextView *bioTextView;
 
 @end
 
@@ -16,8 +22,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//
+//    CGRect frame= self.segmentedControl.frame;
+//    [self.segmentedControl setFrame:CGRectMake(frame.origin.x, frame.origin.y, 400, 50)];
+}
 
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 0;
+}
 
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    return nil;
 }
 
 @end
