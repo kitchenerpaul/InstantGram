57a6da2597329e880953ffbba4ec23fdfe9a4a3e
Paul Kitchener <p.a.kitchener@gmail.com>
2015-10-30 12:16:39 -0500

    added mapviewcontroller

---
+++ InstantGram/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  InstantGram
+//
+//  Created by Paul Kitchener on 10/30/15.
+//  Copyright © 2015 Paul Kitchener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
